775806a49c0594de4c8cd40f86faca7f53861d6b
aminagazizova <aminagazizova8@gmail.com>
2023-11-21 12:54:58 +0300

    sprint 4

--- bookr/urls.py
+++ bookr/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import reviews.views
-from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search/', reviews.views.book_search, name='book_search'),
-    path('', include('reviews.urls')),
+    path("admin/", admin.site.urls),
+    path("", reviews.views.index),
+    path("book-search/", reviews.views.book_search, name="book_search"),
+    path("", include("reviews.urls")),
 ]
--- reviews/models.py
+++ reviews/models.py
@@ -5,11 +5,10 @@ from django.db import models
 class Publisher(models.Model):
     """A company that publishes books."""
     name = models.CharField(max_length=50,
-        help_text="The name of the Publisher.")
-    website = models.URLField(
-        help_text="The Publisher's website.")
+                            help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(
-        help_text="The Publisher's email address.")
+                help_text="The Publisher's email address.")
 
     def __str__(self):
         return self.name
@@ -29,7 +28,7 @@ class Book(models.Model):
                                           through="BookContributor")
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.isbn})"
 
 
 class Contributor(models.Model):
@@ -40,8 +39,15 @@ class Contributor(models.Model):
                                   help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
 
+    def initialled_name(self):
+        """ self.first_names='Jerome David', self.last_names='Salinger'
+            => 'Salinger, JD' """
+        initials = ''.join([name[0] for name
+                            in self.first_names.split(' ')])
+        return "{}, {}".format(self.last_names, initials)
+
     def __str__(self):
-        return self.first_names
+        return self.initialled_name()
 
 
 class BookContributor(models.Model):
@@ -66,3 +72,6 @@ class Review(models.Model):
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE,
                              help_text="The Book that this review is for.")
+
+    def __str__(self):
+        return "{} - {}".format(self.creator.username, self.book.title)
